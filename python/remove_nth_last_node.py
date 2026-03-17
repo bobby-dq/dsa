f03900fecbb87a7aab8bb2a555efd82d18c7b5b4
bobby-dq <josh.quilacio@gmail.com>
2026-03-16 18:57:10 -0600

    added description for the problem

--- python/remove_nth_last_node.py
+++ python/remove_nth_last_node.py
@@ -1,3 +1,20 @@
+"""
+PROBLEM: REMOVE NTH NODE FROM END OF LIST
+
+Statement:
+Given the head of a singly linked list and an integer 'n', remove the 
+nth node from the end of the list and return the head of the modified list.
+
+Constraints:
+- The number of nodes (k) is 1 <= k <= 10^3
+- -10^3 <= Node.value <= 10^3
+- 1 <= n <= k
+
+Technique: Two Pointers (Fast & Slow)
+Time Complexity: O(k) - Single pass
+Space Complexity: O(1) - Constant extra space
+"""
+
 # Definition for a Linked List node
 # class ListNode:
 #     def __init__(self, val=0, next=None):
